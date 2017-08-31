25db53fd300a4f00cabed6c1b4971ad004079138
Anna Ma <annahma@icloud.com>
2017-08-31 15:21:02 -0400

    reverted to static site

--- assets/js.js
+++ assets/js.js
@@ -6,61 +6,6 @@ const codepenTip = document.querySelector('section.connect .showCodepen');
 const twitterTip = document.querySelector('section.connect .showTwitter');
 const emailTip = document.querySelector('section.connect .showEmail');
 
-const convertDate = (date) => {
-	const year = date.substring(0, 4);
-	let month = date.substring(5,7);
-	
-	switch(month) {
-		case "01":
-			month = "Jan";
-			break;
-		case "02":
-			month = "Feb";
-			break;
-		case "03":
-			month = "Mar";
-			break;
-		case "04":
-			month = "Apr";
-			break;
-		case "05":
-			month = "May";
-			break;
-		case "06":
-			month = "Jun";
-			break;
-		case "07":
-			month = "Jul";
-			break;
-		case "08":
-			month = "Aug";
-			break;
-		case "09":
-			month = "Sep";
-			break;
-		case "10":
-			month = "Oct";
-			break;
-		case "11":
-			month = "Nov";
-			break;
-		case "12":
-			month = "Dec";
-			break;
-		default:
-			break;
-	}
-	
-	const day = date.substring(8,10);
-	return `${month} ${day}, ${year}`;
-}
-
-const dates = document.querySelectorAll('section.posts .date');
-
-Object.keys(dates).map((date) => {
-	dates[date].innerText = convertDate(dates[date].innerText);
-});
-
 const showTip = (e) => {
 	const link = e.target.classList;
 	if (link.contains("github")) {
